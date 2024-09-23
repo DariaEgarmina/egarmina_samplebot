00e467b9830bc0f231a383e248b00505b1c66532
Daria Egarmina <d.egarmina@gmail.com>
2024-09-23 13:27:03 +0300

    иниализирует сессию

--- js/index.js
+++ js/index.js
@@ -1,52 +1,65 @@
-import { getDayMenu, getTimeMenu } from "./keyboards.js";
-import { dayTimeResult, addDayTime } from "./db.js";
+import { getDayMenu, getTimeMenu } from './keyboards.js';
+import { dayTimeResult, addDayTime } from './db.js';
 
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import { Context, session, Telegraf } from "telegraf";
+import { session, Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
 
 const { BOT_TOKEN } = process.env;
 const bot = new Telegraf(BOT_TOKEN);
 
-bot.use(session());
+bot.use(
+  session({
+    defaultSession: (ctx) => ({})
+  })
+);
 
 const daysList = {
-  Mon: "понедельник",
-  Tues: "вторник",
-  Wedn: "среду",
-  Thur: "четверг",
-  Fri: "пятницу",
-  Sat: "субботу",
-  Sun: "воскресенье",
+  Mon: 'понедельник',
+  Tues: 'вторник',
+  Wedn: 'среду',
+  Thur: 'четверг',
+  Fri: 'пятницу',
+  Sat: 'субботу',
+  Sun: 'воскресенье',
 };
 
 const timeList = {
-  Seven: "07:00",
-  Twelve: "12:00",
-  Eight: "20:00",
+  Seven: '07:00',
+  Twelve: '12:00',
+  Eight: '20:00',
 };
 
-bot.command("start", (ctx) => {
-  ctx.reply("Выберите день для полива:", getDayMenu());
+bot.command('start', (ctx) => {
+  ctx.reply('Выберите день для полива:', getDayMenu());
 });
 
-bot.action(["Mon", "Tues", "Wedn", "Thur", "Fri", "Sat", "Sun"], (ctx) => {
-  // ctx.session.day = ctx.callbackQuery.data,
+bot.on(message('text'), (ctx) => {
+  ctx.replyWithHTML(
+    'Вы действительно хотите добавить задачу:\n\n' +
+    `<i>${ctx.message.text}</i>`,
 
-  addDayTime(ctx.callbackQuery.data),
+  );
+});
+
+bot.action(['Mon', 'Tues', 'Wedn', 'Thur', 'Fri', 'Sat', 'Sun'], (ctx) => {
+  ctx.session.day = ctx.callbackQuery.data;
+
+  addDayTime(ctx.callbackQuery.data);
   ctx.reply(
     `Вы выбрали ${daysList[ctx.callbackQuery.data]}! Теперь выберите время:`,
     getTimeMenu()
-    );
+  );
 });
 
-bot.action(["Seven", "Twelve", "Eight"], (ctx) => {
-  addDayTime(ctx.callbackQuery.data),
-    ctx.reply(
-      `Отлично!\n\n Я буду напоминать о поливе в  ${timeList[ctx.callbackQuery.data]
-      }`
-    );
+bot.action(['Seven', 'Twelve', 'Eight'], (ctx) => {
+  addDayTime(ctx.callbackQuery.data);
+  ctx.reply(
+    `Отлично!\n\n Я буду напоминать о поливе в ${daysList[ctx.session.day]}  ${timeList[ctx.callbackQuery.data]
+    }`
+  );
 });
 
 bot.launch();
